05f667c59f2c1fcc2209a8443de5fe10b71e57dd
Pawel <szczerbicki.pawel@gmail.com>
2014-12-14 22:44:48 +0100

    remove commen ts

--- src/main/java/com/szczerbicki/clustering/KmeansClustering.java
+++ src/main/java/com/szczerbicki/clustering/KmeansClustering.java
@@ -7,8 +7,8 @@ import java.util.ArrayList;
  */
 public class KmeansClustering {
 
-    private final int NUM_CLUSTERS = 3;    // Total clusters.
-    private final int TOTAL_DATA = 7;      // Total data points.
+    private final int NUM_CLUSTERS = 3;
+    private final int TOTAL_DATA = 7;
 
     private final double SAMPLES[][] = new double[][]{{1.0, 1.0},
             {1.5, 2.0},
@@ -22,21 +22,20 @@ public class KmeansClustering {
     private ArrayList<Centroid> centroids = new ArrayList<>();
 
     private void initialize() {
-        centroids.add(new Centroid(1.0, 1.0)); // lowest set.
-        centroids.add(new Centroid(5.0, 7.0)); // highest set.
+        centroids.add(new Centroid(1.0, 1.0));
+        centroids.add(new Centroid(5.0, 7.0));
     }
 
     public KmeansClustering perform() {
         initialize();
-        final double bigNumber = Math.pow(10, 10);    // some big number that's sure to be larger than our data range.
-        double minimum;                   // The minimum value to beat.
-        double distance;                        // The current minimum value.
+        final double bigNumber = Math.pow(10, 10);
+        double minimum;
+        double distance;
         int sampleNumber = 0;
         int cluster = 0;
         boolean isStillMoving = true;
         Data newData;
 
-        // Add in new data, one at a time, recalculating centroids with each new one.
         while (dataSet.size() < TOTAL_DATA) {
             newData = new Data(SAMPLES[sampleNumber][0], SAMPLES[sampleNumber][1]);
             dataSet.add(newData);
@@ -50,7 +49,6 @@ public class KmeansClustering {
             }
             newData.cluster(cluster);
 
-            // calculate new centroids.
             for (int i = 0; i < NUM_CLUSTERS; i++) {
                 int totalX = 0;
                 int totalY = 0;
@@ -70,27 +68,23 @@ public class KmeansClustering {
             sampleNumber++;
         }
 
-        // Now, keep shifting centroids until equilibrium occurs.
         while (isStillMoving) {
-            // calculate new centroids.
             for (int i = 0; i < NUM_CLUSTERS; i++) {
                 int totalX = 0;
                 int totalY = 0;
                 int totalInCluster = 0;
-                for (Data aDataSet : dataSet) {
+                for (Data aDataSet : dataSet)
                     if (aDataSet.cluster() == i) {
                         totalX += aDataSet.X();
                         totalY += aDataSet.Y();
                         totalInCluster++;
                     }
-                }
                 if (totalInCluster > 0) {
                     centroids.get(i).X(totalX / totalInCluster);
                     centroids.get(i).Y(totalY / totalInCluster);
                 }
             }
 
-            // Assign all data to the new centroids
             isStillMoving = false;
 
             for (Data tempData : dataSet) {
