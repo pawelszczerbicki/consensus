4f5209162d67a96806efeda9cff6e192d370b7fa
shark <szczerbicki.pawel@gmail.com>
2015-04-21 22:20:01 +0200

    fix startingn consensus alg

--- standalone/src/main/java/com/szczerbicki/Main.java
+++ standalone/src/main/java/com/szczerbicki/Main.java
@@ -3,12 +3,10 @@ package com.szczerbicki;
 import com.szczerbicki.task.TaskService;
 import com.szczerbicki.task.TaskWorker;
 
-import static java.lang.Integer.parseInt;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello clustering");
-        new TaskWorker(new TaskService()).start(parseInt(args[0]), parseInt(args[1]));
+        new TaskWorker(new TaskService()).start();
     }
 }
